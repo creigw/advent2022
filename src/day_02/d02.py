66b1a683b4f8a36e2a3d702b5b6f678f4fc40fd7
Wesley Creighton <creigw@amazon.com>
2022-12-21 16:33:05 -0800

    D02 P2 Complete

--- src/day_02/d02.py
+++ src/day_02/d02.py
@@ -23,10 +23,16 @@ WIN_REF = {
     'C': 'X'
 }
 
+LOSS_REF = {
+    'A': 'Z',
+    'B': 'X',
+    'C': 'Y'
+}
+
 CHOICE_DECODE = {
-    'A': 'X',
-    'B': 'Y',
-    'C': 'Z'
+    'A': 'X',  # rock
+    'B': 'Y',  # paper
+    'C': 'Z'  # scissors
 }
 
 
@@ -47,7 +53,7 @@ class Round():
         return ChoiceScore[self.our_choice].value + self.round_outcome().value
 
 
-def get_round_list(data: List[str]) -> List[Round]:
+def get_round_list_p1(data: List[str]) -> List[Round]:
     round_list = []
 
     for round in data:
@@ -63,28 +69,57 @@ def get_rounds_total_score(round_list: List[Round]):
         total_score += round.result_score()
     return total_score
 
+
+# Part 2
+######################
+OUTCOME_DECODE = {
+    'X': RoundOutcome.LOSS,
+    'Y': RoundOutcome.DRAW,
+    'Z': RoundOutcome.WIN
+}
+
+
+def get_choices_given_outcome(coded_values):
+    opponent_choice = coded_values[0]
+    outcome = OUTCOME_DECODE[coded_values[1]]
+
+    if outcome == RoundOutcome.DRAW:
+        our_choice = CHOICE_DECODE[opponent_choice]
+    elif outcome == RoundOutcome.LOSS:
+        our_choice = LOSS_REF[opponent_choice]
+    elif outcome == RoundOutcome.WIN:
+        our_choice = WIN_REF[opponent_choice]
+    return (opponent_choice, our_choice)
+
+
+def get_round_list_p2(data: List[str]) -> List[Round]:
+    round_list = []
+
+    for round in data:
+        coded_values = round.split()
+        opponent_choice, our_choice = get_choices_given_outcome(coded_values)
+        round_list.append(Round(opponent_choice, our_choice))
+
+    return round_list
+
 ######################
 
 
-def main(data: List, debug):
-
-    round_list = get_round_list(data)
-
-    if debug:
-        for r in round_list:
-            print(r.round_outcome())
-            print(r.result_score())
+def main(data: List):
 
     # Part 1
-    total_rounds_score = get_rounds_total_score(round_list)
-    print(f'The total rounds score is: {total_rounds_score}')
+    round_list_p1 = get_round_list_p1(data)
+    total_rounds_score = get_rounds_total_score(round_list_p1)
+    print(f'The total rounds score for part 1 is: {total_rounds_score}')
 
     # Part 2
+    round_list_p2 = get_round_list_p2(data)
+    total_rounds_score = get_rounds_total_score(round_list_p2)
+    print(f'The total rounds score for part 2 is: {total_rounds_score}')
 
 
 if __name__ == '__main__':
     # Config run
-    DEBUG = True
     IS_TEST = 0
 
     DAY = 'd02'
@@ -96,4 +131,4 @@ if __name__ == '__main__':
 
     data = read_file_to_string_list(path)
 
-    main(data, DEBUG)
+    main(data)
